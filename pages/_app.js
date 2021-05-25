fed3f311fa30e17cc87f87329329c8a8e94c9c9f
Bekir <l3ekirerdem@gmail.com>
2021-05-26 00:03:33 +0300

    auth0 entegrasyonu

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import "../styles/global.css";
 import Header from "../components/index";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Auth0Provider
+    domain="dev-2pr2o4rg.us.auth0.com"
+    clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+    redirectUri={process.env.NEXT_PUBLIC_URL}
+   >
     <div className="antialiased text-gray-700">
       <Header />
       <main className="mt-5">
         <Component {...pageProps} />
       </main>
     </div>
+    </Auth0Provider>
   );
 }
 
